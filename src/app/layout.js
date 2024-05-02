b21210f84b162a7bec217e28fc873d0890b6a8d3
Victor Merseguel <merseguel@gmail.com>
2024-05-02 19:18:46 +0100

    tagmanager

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,36 +25,30 @@ export default function RootLayout({ children }) {
         name="facebook-domain-verification"
         content="ugxw5pa7vnyw1iqdch6xp0ccnu7khh"
       />
-      {/* <!-- Meta Pixel Code --> */}
       <Script
-        id="fb-pixel"
+        id="goole_tagManager"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-        !function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1638861453554103');
-fbq('track', 'PageView');`,
+          function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5F5RCL59'`,
         }}
       />
-      <noscript>
-        <img
-          height="1"
-          width="1"
-          style="display:none"
-          src="https://www.facebook.com/tr?id=1638861453554103&ev=PageView&noscript=1"
-        />
-      </noscript>
-      {/* <!-- End Meta Pixel Code --> */}
+
       <body
         className={`${FilsonPro.variable} ${LeagueSpartan.variable} ${HeadLineA.variable} ${YellowTail.variable} ${NewOrder.variable}`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5F5RCL59"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
         <ContextProvider>{children}</ContextProvider>
       </body>
     </html>
