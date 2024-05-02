f79a9d2868490170c56a573297139086a34343a2
Victor Merseguel <merseguel@gmail.com>
2024-05-02 19:38:21 +0100

    add tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,29 +26,21 @@ export default function RootLayout({ children }) {
         content="ugxw5pa7vnyw1iqdch6xp0ccnu7khh"
       />
       <Script
-        id="goole_tagManager"
+        id="google-tag-manager"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-          function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5F5RCL59'`,
+          })(window,document,'script','dataLayer','GTM-5F5RCL59');`,
         }}
-      />
+      ></Script>
 
       <body
         className={`${FilsonPro.variable} ${LeagueSpartan.variable} ${HeadLineA.variable} ${YellowTail.variable} ${NewOrder.variable}`}
       >
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5F5RCL59"
-            height="0"
-            width="0"
-            style="display:none;visibility:hidden"
-          ></iframe>
-        </noscript>
         <ContextProvider>{children}</ContextProvider>
       </body>
     </html>
