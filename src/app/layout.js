4be664cbd8a6d828b1d9276cbe841f0ed25fa2dc
Victor Merseguel <merseguel@gmail.com>
2024-05-20 11:58:45 +0100

    add registration pixel

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,29 +38,29 @@ export default function RootLayout({ children }) {
         }}
       ></Script>
       <Script
-        id="google-tag-manager-server"
+        id="google-tag-manager"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5F5RCL59');`,
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5F5RCL59');`,
         }}
       ></Script>
 
       <body
         className={`${FilsonPro.variable} ${LeagueSpartan.variable} ${HeadLineA.variable} ${YellowTail.variable} ${NewOrder.variable}`}
       >
-        <noscript>
+        {/* <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5F5RCL59"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MCM7L6GP"
             height="0"
             width="0"
             style="display:none;visibility:hidden"
           ></iframe>
-        </noscript>
+        </noscript> */}
         <ContextProvider>{children}</ContextProvider>
       </body>
     </html>
